8dda37a8fd0d2977424f7af7cace85c4e408e211
ss19698 <shailysahu1972006@gmail.com>
2026-01-06 19:01:19 +0530

    deploy

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // important! makes JS/CSS paths relative
-  build: {
-    outDir: 'dist', // your build folder
-    sourcemap: false
-  }
+  base: './',      
+  build: { outDir: 'dist' }
 });
